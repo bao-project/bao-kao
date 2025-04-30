4d0aa2046800745d2a34d5bbda8fe58e594138f1
Diogo Costa <diogoandreveigacosta@gmail.com>
2025-04-30 14:11:13 +0100

    fix(launch.sh): update qemu scripts to support different OSes
    Signed-off-by: Diogo Costa <diogoandreveigacosta@gmail.com>

--- framework/launch/qemu-riscv64-virt.sh
+++ framework/launch/qemu-riscv64-virt.sh
@@ -2,29 +2,34 @@
 #!nix-shell --pure -p unixtools.netstat -p qemu -i bash
 
 # Check if a version argument is provided
-if [ -z "$2" ]; then
-    echo "Usage: $0 <qemu-platform> <opensbi_elf_path> <bao-bin-path>"
+if [ -z "$3" ]; then
+    echo "Usage: $0 <qemu-platform> <opensbi_elf_path> <bao-bin-path> <guest_OS>"
     exit 1
 fi
 
 qemu_platform="$1"
 opensbi_elf_path="$2"
 bao_bin_path="$3"
+guest_OS="$4"
 
 if netstat -tuln | grep ":5555 " &>/dev/null; then
     exit -1
 fi
 
-qemu_stderr=$(mktemp)
+if [[ "$guest_OS" == "linux" ]]; then
+    extra_serial_args="-device virtio-serial-device -chardev pty,id=serial3 -device virtconsole,chardev=serial3 -serial pty"
+else
+    extra_serial_args="-serial pty"
+fi
 
-qemu-system-riscv64 -nographic \
+qemu-system-"$qemu_platform" -nographic \
     -M virt -cpu rv64 -m 4G -smp 4 \
-    -bios $opensbi_elf_path \
+    -bios "$opensbi_elf_path" \
     -device loader,file="$bao_bin_path",addr=0x80200000,force-raw=on \
     -device virtio-net-device,netdev=net0 \
     -netdev user,id=net0,net=192.168.42.0/24,hostfwd=tcp:127.0.0.1:5555-:22 \
-    -device virtio-serial-device -chardev pty,id=serial3 -device virtconsole,chardev=serial3 \
-    -serial pty 2> "$qemu_stderr"
-    
+    $extra_serial_args \
+    2> "$qemu_stderr"
+
 rm "$qemu_stderr"
 exit 0
