075e900440fe375a2be351e6a89f44f884b54931
Diogo Costa <diogoandreveigacosta@gmail.com>
2025-04-30 14:11:13 +0100

    fix(launch.sh): update qemu scripts to support different OSes
    Signed-off-by: Diogo Costa <diogoandreveigacosta@gmail.com>

--- framework/launch/qemu-aarch64-virt.sh
+++ framework/launch/qemu-aarch64-virt.sh
@@ -3,7 +3,7 @@
 
 # Check if a version argument is provided
 if [ -z "$4" ]; then
-    echo "Usage: $0 <qemu-platform> <flash_bin_path> <bao-bin-path> <gic_version>"
+    echo "Usage: $0 <qemu-platform> <flash_bin_path> <bao-bin-path> <gic_version> <guest_OS>"
     exit 1
 fi
 
@@ -11,12 +11,20 @@ qemu_platform="$1"
 flash_bin_path="$2"
 bao_bin_path="$3"
 gic_version="$4"
+guest_OS="$5"
 
 if netstat -tuln | grep ":5555 " &>/dev/null; then
+    echo "Port 5555 is already in use"
     exit -1
 fi
 
 qemu_stderr=$(mktemp)
+
+if [[ "$guest_OS" == "linux" ]]; then
+  extra_serial_args="-device virtio-serial-device -chardev pty,id=serial3 -device virtconsole,chardev=serial3 -serial pty"
+else
+  extra_serial_args="-serial pty"
+fi
 
 qemu-system-"$qemu_platform" -nographic \
     -M virt,secure=on,virtualization=on,gic-version=$gic_version \
@@ -24,7 +32,9 @@ qemu-system-"$qemu_platform" -nographic \
     -bios "$flash_bin_path" \
     -device loader,file="$bao_bin_path",addr=0x50000000,force-raw=on \
     -device virtio-net-device,netdev=net0 -netdev user,id=net0,hostfwd=tcp:127.0.0.1:5555-:22 \
-    -serial pty 2> "$qemu_stderr"
+    $extra_serial_args \
+    2> "$qemu_stderr"
+
 
 rm "$qemu_stderr"
 exit 0
